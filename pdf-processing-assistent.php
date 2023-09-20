f425b3d316e755f6c7a41d44aac9397a558fd557
varysstorm <pa.maniura@googlemail.com>
2023-09-20 18:17:16 +0200

    update

--- pdf-processing-assistent.php
+++ pdf-processing-assistent.php
@@ -1,18 +1,21 @@
 <?php
 /*
 Plugin Name: ITNT PDF Processing Assistent
-Description: Automatischer Assistent zur Beitragsgenerierung aus PDFs.
+Plugin URI: ''
+Description: Automatischer Assistent zur Beitragsgenerierung aus PDFs. Dieses Plugin ermöglicht es Benutzern, ...
 Version: 0.1
 Author: Patrick Maniura
+Author URI: ''
+License: GPL-2.0+
 Text Domain: ITNT PDF Processing Assistent
-
+Domain Path: ''
+Stable Tag: 0.1
 */
 
-//require __DIR__ . 'ppa-backend.php';
-
+require __DIR__ . '/ppa-backend.php';
 
 function itnt_ppa_activate() {
 }
 register_activation_hook(__FILE__, 'itnt_ppa_activate');
-
 ?>
+
