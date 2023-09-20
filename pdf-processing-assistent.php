6e760b2efbc1f255295a6bfa4d91a27bd49c02c1
varysstorm <pa.maniura@googlemail.com>
2023-09-20 18:12:51 +0200

    update

--- pdf-processing-assistent.php
+++ pdf-processing-assistent.php
@@ -8,12 +8,10 @@ Text Domain: ITNT PDF Processing Assistent
 
 */
 
-require __DIR__ . 'ppa-backend.php';
+//require __DIR__ . 'ppa-backend.php';
 
 
 function itnt_ppa_activate() {
-    wp_register_style('itnt_pdf_processing_assistent', plugins_url('css/style.css');
-    wp_enqueue_style('wp_register_style');
 }
 register_activation_hook(__FILE__, 'itnt_ppa_activate');
 
