a63bbcb0ba5073389c3765aa2177f7a3855e160f
VarysStorm <111200466+VarysStorm@users.noreply.github.com>
2023-10-10 02:28:13 -0700

    Update ppa-backend.php

--- ppa-backend.php
+++ ppa-backend.php
@@ -6,18 +6,32 @@ function itnt_ppa_menu()
         'ITNT PPA', 
         'manage_options', 
         'itnt_ppa', 
-        'itnt_ppa_main', 
-        99 
+        'itnt_ppa_main' ,
     );
     
 }
 add_action('admin_menu', 'itnt_ppa_menu');
 
-function itnt_ppa_main()
-{
-    
-    echo '<div class="wrap"><h2>Dein Plugin</h2></div>';
+function itnt_ppa_main(){
+
+
+    wp_register_style( 'ppa_css', plugins_url('/css/style.css', __FILE__), array() );
+    wp_enqueue_style( 'ppa_css' );
+
+    wp_register_script('ppa-backend_script', plugins_url('/js/ppa-backend.js', __FILE__), array());
+    wp_register_script('pdfjs', plugins_url('/js/pdfjs-3.11.174-dist/build/pdf.js', __FILE__), array(), '3.11.174', false);
+
+    wp_enqueue_script('pdfjs');
+    wp_enqueue_script('ppa-backend_script');
+
+    $output = '';
+    $output .= '<div class="plugin-title">';
+        $output .= '<div class="wrapper">';
+            $output .= '<h1>ITNT PPA</h1>';
+        $output .= '</div>'; 
+    $output .= '</div>';
+
+
+    echo $output;
 }
-
-
 ?>
