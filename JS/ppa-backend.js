b7b531440a262160fc57239898a25353c6e45a16
varysstorm <pa.maniura@googlemail.com>
2023-10-10 14:44:05 -0700

    Upload Form

--- JS/ppa-backend.js
+++ JS/ppa-backend.js
@@ -1,7 +1,15 @@
 jQuery( document ).ready(function() {
     console.log( "test" );
 
-    jQuery('#ppa-w-b-1').html('Upload Box');
+    jQuery('#ppa-w-b-1').html(uploadConsturctor());
     jQuery('#ppa-w-b-2').html('PDF Canva Box');
     jQuery('#ppa-w-b-3').html('Beitrags Generierung Box');
 });
+
+function uploadConsturctor(){
+    html = '';
+    html += '<form action="#" method="post" enctype="multipart/form-data">';
+        html += '<input type="file" name="pdf_file" accept=".pdf">';
+        html += '<input type="submit" value="PDF hochladen">';
+    html += '</form>';
+}
