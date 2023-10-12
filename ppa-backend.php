71232e760c3bd66ebe0cc80e92c18bfd3884e770
varysstorm <pa.maniura@googlemail.com>
2023-10-12 15:53:56 +0200

    Update

--- ppa-backend.php
+++ ppa-backend.php
@@ -16,7 +16,6 @@ function itnt_ppa_main(){
 
 
     wp_register_style( 'ppa_css', plugins_url('/css/style.css', __FILE__), array() );
-    wp_register_style( 'ppa_css', plugins_url('/css/style.css', __FILE__), array() );
     $font_awesome_cdn = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css';
 
     wp_enqueue_style('font-awesome', $font_awesome_cdn, array(), '5.15.3');
@@ -24,11 +23,12 @@ function itnt_ppa_main(){
 
     wp_register_script('ppa-backend_script', plugins_url('/js/ppa-backend.js', __FILE__), array());
     wp_register_script('pdfjs', plugins_url('js/pdf.js/src/pdf.js', __FILE__), array());
-
+    $pdfjs_url = plugins_url('js/pdf.js/src/pdf.js', __FILE__);
     wp_enqueue_script('pdfjs');
     wp_enqueue_script('ppa-backend_script');
 
     $output = '';
+    $output = "<script src='$pdfjs_url'></script>";
     $output .= '<div class="plugin-title">';
         $output .= '<div class="wrapper">';
             $output .= '<h1>ITNT PPA</h1>';
